2a2780eb9442a2d0c2b9dc5defa43c0c7b7d93be
Diego Berrios <diegus99@gmail.com>
2024-05-19 19:04:10 -0400

    Changed HoverMenu component

--- frontend/src/components/general/HoverMenu.jsx
+++ frontend/src/components/general/HoverMenu.jsx
@@ -14,8 +14,8 @@ const HoverMenu = ({children, hoverMenu}) => {
       </div>
       {
         isHovered &&
-        <div className='absolute left-0 md:left-auto md:absolute top-14 md:top-auto md:-bottom-20 border-b-2 md:border-2 border-gray-200 md:rounded-xl w-screen md:w-20 md:h-20'>
-          TO-DO
+        <div className='absolute left-0 md:left-auto md:absolute top-14 md:top-auto md:-bottom-24 border-b-2 md:border-2 border-gray-200 md:rounded-xl w-screen md:w-20 md:h-20'>
+          <div className='absolute -top-2 right-1/2 border-x-8 border-b-8 border-x-transparent border-slate-200'/>
         </div>
       }
     </div>
