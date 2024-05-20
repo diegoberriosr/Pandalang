590967b475a0b1c78170ff6595f37c847fc07a0b
Diego Berrios <diegus99@gmail.com>
2024-05-20 12:43:53 -0400

    Completed Quests view layout

--- frontend/src/views/GeneralView.tsx
+++ frontend/src/views/GeneralView.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Leaderboard = ({header, subheader, icon, children}) => {
+const GeneralView = ({header, subheader, icon, children}) => {
   return (
-    <div className='absolute top-14 md:relative md:top-auto py-5 w-full md:w-7/12 flex flex-col items-center space-y-5'>
+    <div className='absolute top-14 md:relative md:top-auto py-5 md:pt-10 w-full md:w-7/12 flex flex-col items-center space-y-5'>
         <img src={icon} alt='shop' className='w-16 h-16'/>
         <h4 className='text-2xl text-slate-800 font-bold'>{header}</h4>
         <h5 className='text-lg text-slate-500'>{subheader}</h5>
@@ -11,4 +11,4 @@ const Leaderboard = ({header, subheader, icon, children}) => {
   )
 }
 
-export default Leaderboard
+export default GeneralView;
--- frontend/src/views/Quests.tsx
+++ frontend/src/views/Quests.tsx
@@ -1,10 +1,42 @@
 import React from "react"
 
+// Icon imports
+import Medal from '../assets/elements/medal.png';
+import Thunder from '../assets/elements/thunder.png';
+
+// Component imports
+import GeneralView from './GeneralView.tsx';
+import ViewListElement from './ViewListElement.tsx';
+
+type Quest = {
+  title : string,
+  xp : number
+}
+
+const TEST_QUESTS : [Quest] = [
+  { title : 'Earn 10 xp', xp : 10},
+  { title : 'Earn 100 xp', xp : 100},
+  { title : 'Earn 1000 xp', xp : 1000},
+  { title : 'Earn 10000 xp', xp: 10000},
+  { title : 'Earn 100000 xp', xp: 100000},
+  { title : 'Earn 1000000 xp', xp : 1000000}
+];
+
 const Quests = () => {
   return (
-    <div className='absolute top-14 md:relative md:top-auto py-5 w-full md:w-7/12 flex flex-col items-center space-y-5'>
-      Quests
-    </div>
+    <GeneralView header='Quests' subheader='Complete quests by earning xp' icon={Medal}>
+      <ul className='w-screen md:w-8/12'>
+        {TEST_QUESTS.map( quest => <ViewListElement styling=''>
+          <div className="flex items-center space-x-3 w-full">
+            <img src={Thunder} alt='thunder' className='w-8 h-8'/>
+            <div className= 'w-full'>
+              <h4 className='text-lg text-slate-800 font-bold'>{quest.title}</h4>
+              <progress max={quest.xp}  value={10} className='w-full bg-slate-200 rounded'/>
+            </div>
+          </div>
+        </ViewListElement>)}
+      </ul>
+    </GeneralView>
   )
 }
 
