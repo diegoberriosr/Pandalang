76385ea8031113e93a2a8a0a5fae72b51d8bea3b
Diego Berrios <diegus99@gmail.com>
2024-05-31 15:00:16 -0400

    Modified Shop component

--- frontend/src/components/shop/Shop.tsx
+++ frontend/src/components/shop/Shop.tsx
@@ -1,5 +1,6 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // Icon imports
 import ShopImg from '../../assets/elements/shop.png';
@@ -13,21 +14,14 @@ import GeneralView from '../../views/GeneralView.tsx';
 import ViewListElement from '../../views/ViewListElement.tsx';
 
 // Context imports
-import { AuthContext } from '../../context/AuthContext.tsx';
+import { StatusContext } from '../../context/StatusContext.tsx';
 
 const Shop = () => {
-  const { user, setUser } = React.useContext(AuthContext); // Call the Authcontext to dinamically display/modify user data
-  
-  // Refill a certain amount of hearts
-  const handleRefill = (hearts_refilled, xp_amount) => { 
-    if (user.available_xp < xp_amount || user.hearts + hearts_refilled > 5) return; // Only valid when user has enough available xp / heart counter is not full
-    setUser({...user, hearts : user.hearts + hearts_refilled, available_xp : user.available_xp - xp_amount}); // Update user
-  };
+  const [loading, setLoading] = useState(false);
+  const { status, handleTransaction } = React.useContext(StatusContext); // Call the StatusContext to dinamically display/modify user status data
 
-  // Updates membership status
-  const updateMembershipStatus = () => { 
-    setUser({...user, isPremium: !user.isPremium}); // Update user's membership status
-  };
+  const navigate = useNavigate();
+  
 
   return (
     <GeneralView header='Shop' subheader='Refill your hearts with xp' icon={ShopImg} >
@@ -37,9 +31,9 @@ const Shop = () => {
                     <img src={Heart} alt='heart' className='w-12 h-12'/>
                     <span className='text-slate-700 text-lg font-semibold ml-5'>Refill Hearts</span>
                 </div>
-                <Button size='sm' disabled={ user.hearts === 5 || user.available_xp < 50 || user.isPremium}
-                    onClick = {() => handleRefill(5- user.hearts, 50)}>
-                    { user.hearts === 5 ? 'Full' : <span className='flex items-center space-x-1'>
+                <Button size='sm' disabled={ status.hearts === 5 || status.available_xp < 50 || status.isPremium}
+                    onClick = {() => handleTransaction(50, 5- status.hearts, setLoading)}>
+                    { status.hearts === 5 ? 'Full' : <span className='flex items-center space-x-1'>
                             <img src={Thunder} alt='thunder' className='w-3 h-3'/>
                             <span>50 XP</span>
                         </span>}
@@ -50,9 +44,9 @@ const Shop = () => {
                     <img src={Heart} alt='heart' className='w-12 h-12'/>
                     <span className='text-slate-700 text-lg font-semibold ml-5'>Refill 1 heart</span>
                 </div>
-                <Button size='sm' disabled={ user.hearts === 5 || user.available_xp < 20 || user.isPremium}
-                onClick = {() => handleRefill(1, 20)}>    
-                    { user.hearts === 5 ? 'Full' : <span className='flex items-center space-x-1'>
+                <Button size='sm' disabled={ status.hearts === 5 || status.available_xp < 20 || status.isPremium}
+                onClick = {() => handleRefill(20, 1, setLoading)}>    
+                    { status.hearts === 5 ? 'Full' : <span className='flex items-center space-x-1'>
                             <img src={Thunder} alt='thunder' className='w-3 h-3'/>
                             <span>20 XP</span>
                         </span>}
@@ -64,8 +58,8 @@ const Shop = () => {
                     <span className='text-slate-700 text-lg font-semibold ml-5'>Get unlimited hearts</span>
                 </div>
                 <Button size='sm'
-                onClick={updateMembershipStatus}>
-                    { user.isPremium ? 'Cancel' : 'Upgrade'}
+                onClick={() => navigate('/premium')}>
+                    { status.isPremium ? 'Cancel' : 'Upgrade'}
                 </Button>
             </ViewListElement>
         </ul>
